c840b0b9848651c38f7d1f1e2e4cf8c7bf440a2d
Lewis Lovett <llovett7575@gmail.com>
2022-10-18 14:36:26 +0100

    Created Ticket Component

---
+++ ticket-tracker/src/components/Ticket/Ticket.jsx
@@ -0,0 +1,12 @@
+import "./Ticket.scss";
+import Counter from "../Counter/Counter";
+
+const Ticket = (props) => {
+    return(
+        <>
+            <h1>Name</h1>
+            <h2>Role</h2>
+            <Counter/>
+        </>
+    )
+}
