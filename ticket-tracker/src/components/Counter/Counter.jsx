f0640a482bd1df594e31b63d4892f4b2922dc39f
Lewis Lovett <llovett7575@gmail.com>
2022-10-18 14:34:44 +0100

    Created Counter component

---
+++ ticket-tracker/src/components/Counter/Counter.jsx
@@ -0,0 +1,14 @@
+import "./Counter.scss";
+
+const Counter = (props) => {
+    return (
+        <>
+            <h1>Counter</h1>
+            <button>-</button>
+            <div>0</div>
+            <button>+</button>
+        </>
+    );
+};
+
+export default Counter;
