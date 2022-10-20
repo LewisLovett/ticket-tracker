b77c31f567e959e40ad47dce73b254143d9cdea2
Lewis Lovett <llovett7575@gmail.com>
2022-10-20 16:34:35 +0100

    Added Dropdown functionality

--- ticket-tracker/src/containers/TicketContainer/TicketContainer.jsx
+++ ticket-tracker/src/containers/TicketContainer/TicketContainer.jsx
@@ -8,13 +8,11 @@ const TicketContainer = (props) => {
         ))
     );
 
-   
-
     const handleSearchInput = (event) =>{
 
         const ticketName = event.target.value;
         const lenOfInput = ticketName.length;
-        const filteredArry = (teamArry.filter(ticket=> ticket.name.substring(0,lenOfInput) == ticketName.substring(0,lenOfInput)));
+        const filteredArry = (teamArry.filter(ticket=> ticket.name.substring(0,lenOfInput).toLowerCase() == ticketName.substring(0,lenOfInput).toLowerCase()));
         setTicketListJSX(
             filteredArry.map((ticket) => (
                 <Ticket ticketName = {ticket.name} ticketRole = {ticket.role}/>
@@ -24,9 +22,32 @@ const TicketContainer = (props) => {
         
     }
 
+    const roleDropdownJSX = (
+        teamArry.map((ticket) => (
+            <option value={ticket.role}>{ticket.role}</option>
+        ))
+    )
+
+    
+    const handleRoleChange = (event) =>{
+
+            const ticketRole = event.target.value;
+            const filteredArry = (teamArry.filter(ticket=> ticket.role == ticketRole));
+            setTicketListJSX(
+                filteredArry.map((ticket) => (
+                    <Ticket ticketName = {ticket.name} ticketRole = {ticket.role}/>
+                ))
+            )
+            
+            
+    }
+
     const ticketContainerJSX = (
         <>
         <input className='nameSearch' onInput={handleSearchInput}/>
+        <select name="roles" onChange={handleRoleChange}>
+            {roleDropdownJSX}
+        </select>
         <div className="ticketContainer">
             {ticketListJSX}
         </div>
